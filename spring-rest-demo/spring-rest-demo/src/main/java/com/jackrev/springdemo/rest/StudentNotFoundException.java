7a24bbfebb9d2a53fc2b333c596f67f4e1424820
Abhay Sudan <abhaysudan007@gmail.com>
2020-04-28 21:25:52 +0530

    spring-rest-demo exception handling

---
+++ spring-rest-demo/spring-rest-demo/src/main/java/com/jackrev/springdemo/rest/StudentNotFoundException.java
@@ -0,0 +1,17 @@
+package com.jackrev.springdemo.rest;
+
+public class StudentNotFoundException extends RuntimeException {
+
+	public StudentNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public StudentNotFoundException(String message) {
+		super(message);
+	}
+
+	public StudentNotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+}
